de7850f7ee4c72cb1419e40df50d0c8be35f6c8a
thuythaovy <nguyenthuythaovy3105@gmail.com>
2024-08-25 11:53:39 +0700

    Add files via upload

---
+++ giatribieuthuc.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 09:09:01 2024
+
+@author: Student
+"""
+
+a=float(input("Nhập số thực a:"))
+b=float(input("Nhập số thực b:"))
+x=((a**(1/2)-b**(1/2))/(a**(1/4)-b**(1/4)))-((a**(1/2)+(a*b)**(1/4))/(a**(1/4)+b**(1/4)))
+print("Kết quả:",x)
